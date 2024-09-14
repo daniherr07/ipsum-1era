d00654a551c65854c71cc362c1c786b4a008eb00
maindream <dherrera2195@gmail.com>
2024-09-14 12:46:15 -0600

    Suspsense 2

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,18 +1,20 @@
 import NavBar from "../components/navbar/Navbar";
 import MainSearch from "./components/MainSearch";
 import style from './search.module.css'
-
+import { Suspense } from 'react'
 
 export default function Search(){
 
 
     return(
         <>
-            <NavBar></NavBar>
-            <main className={style.main}>
-                <MainSearch></MainSearch>
+            <Suspense>
+                <NavBar></NavBar>
+                <main className={style.main}>
+                    <MainSearch></MainSearch>
 
-            </main>
+                </main>
+            </Suspense>
         </>
 
     );
