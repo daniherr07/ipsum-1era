bf475f3e3f19f4411bcc300d0c15a7f613d1bcc9
Daniel Gutierrez <dherrera2195@gmail.com>
2025-02-20 19:13:38 -0600

    Arreglar problema access tokens

--- src/app/api/deleteBlob/route.js
+++ src/app/api/deleteBlob/route.js
@@ -1,8 +1,34 @@
 import { NextResponse } from 'next/server';
 import dropboxV2Api from 'dropbox-v2-api';
+import axios from 'axios'
+const CLIENT_ID = process.env.DROPBOX_CLIENT_ID;
+const CLIENT_SECRET = process.env.DROPBOX_CLIENT_SECRET;
+const REFRESH_TOKEN = process.env.DROPBOX_REFRESH_TOKEN;
+
+async function getAccessToken() {
+    try {
+        const response = await axios.post("https://api.dropbox.com/oauth2/token", new URLSearchParams({
+            refresh_token: REFRESH_TOKEN,
+            grant_type: "refresh_token",
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET
+        }).toString(), {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        });
+
+        console.log("Nuevo Access Token:", response.data.access_token);
+        return response.data.access_token;
+    } catch (error) {
+        console.error("Error obteniendo el access token:", error.response.data);
+        return null;
+    }
+}
+
+// Ejecutar para obtener un nuevo token
+const token = await getAccessToken();
 
 const dropbox = dropboxV2Api.authenticate({
-  token: process.env.DROPBOX_ACCESS_TOKEN,
+  token: token,
 });
 
 export async function DELETE(request) {
